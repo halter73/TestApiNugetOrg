a63272fad97d3d5644d2935dda081b2a15bcb223
Stephen Halter <halter73@gmail.com>
2020-09-08 12:06:06 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestApiNugetOrg
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var client = new HttpClient();
+
+            var response = await client.GetStringAsync("https://api.nuget.org/v3/index.json");
+            //var response = await client.GetStringAsync("https://google.com");
+
+            Console.WriteLine("Received response");
+            Console.WriteLine();
+            Console.WriteLine(response);
+        }
+    }
+}
